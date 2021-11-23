3feaaf015591b16e2aa8dbb656246b299ddd2378
Anna Karolyne <dev.akvc@gmail.com>
2021-11-23 10:47:20 -0300

    Criar entidade de Lead

---
+++ Lead-Collector/src/main/java/br/com/zup/LeadCollector/lead/Lead.java
@@ -0,0 +1,44 @@
+package br.com.zup.LeadCollector.lead;
+
+import br.com.zup.LeadCollector.produto.Produto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "leads")
+public class Lead {
+    @Id
+    private String email;
+    @Column(columnDefinition = "VARCHAR(100) DEFAULT 'Nao informado'")
+    private String nome;
+    private List<Produto> produtosDeInteresse;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutosDeInteresse() {
+        return produtosDeInteresse;
+    }
+
+    public void setProdutosDeInteresse(List<Produto> produtosDeInteresse) {
+        this.produtosDeInteresse = produtosDeInteresse;
+    }
+
+}
