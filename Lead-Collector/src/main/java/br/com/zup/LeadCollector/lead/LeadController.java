054860afb79f9349474fc18f70f4be7a5648d5d2
Anna Karolyne <dev.akvc@gmail.com>
2021-11-23 10:54:25 -0300

    Criar metodo put para salvar Lead

--- Lead-Collector/src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
+++ Lead-Collector/src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
@@ -1,6 +1,8 @@
 package br.com.zup.LeadCollector.lead;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeadController {
     @Autowired
     private LeadService leadService;
+
+    @PutMapping
+    public Lead cadastrarLead(@RequestBody Lead lead){
+        return leadService.salvarLead(lead);
+    }
 
 }
